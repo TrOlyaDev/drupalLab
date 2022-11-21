8c1e6753e0d1b898f5a4d2101fb4872744472b6e
Olha Samoilova <trole4ka@gmail.com>
2022-11-21 14:48:44 +0200

    [Task 17] Student entity+CRUD

---
+++ web/modules/custom/student/src/Entity/Student.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace Drupal\student\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\student\StudentInterface;
+
+/**
+ * Defines the student entity class.
+ *
+ * @ContentEntityType(
+ *   id = "student",
+ *   label = @Translation("Student"),
+ *   label_collection = @Translation("Students"),
+ *   label_singular = @Translation("student"),
+ *   label_plural = @Translation("students"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count students",
+ *     plural = "@count students",
+ *   ),
+ *   handlers = {
+ *     "list_builder" = "Drupal\student\StudentListBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "form" = {
+ *       "add" = "Drupal\student\Form\StudentForm",
+ *       "edit" = "Drupal\student\Form\StudentForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm",
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     }
+ *   },
+ *   base_table = "student",
+ *   admin_permission = "administer student",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "id",
+ *     "uuid" = "uuid",
+ *   },
+ *   links = {
+ *     "collection" = "/admin/content/student",
+ *     "add-form" = "/student/add",
+ *     "canonical" = "/student/{student}",
+ *     "edit-form" = "/student/{student}/edit",
+ *     "delete-form" = "/student/{student}/delete",
+ *   },
+ * )
+ */
+class Student extends ContentEntityBase implements StudentInterface {
+
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Name'))
+      ->setRequired(TRUE)
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'inline',
+        'type' => 'string',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['surname'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Surname'))
+      ->setRequired(TRUE)
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'inline',
+        'type' => 'string',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['email'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Email'))
+      ->setRequired(TRUE)
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'inline',
+        'type' => 'string',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['phone'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Phone'))
+      ->setRequired(TRUE)
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'inline',
+        'type' => 'string',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['age'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Age'))
+      ->setRequired(TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'numeric',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'inline',
+        'type' => 'numeric',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['gender'] = BaseFieldDefinition::create('list_string')
+      ->setLabel(t('Gender'))
+      ->setRequired(TRUE)
+      ->setSettings([
+        'allowed_values' => [
+          'Male' => 'Male',
+          'Female' => 'Female',
+          'Other' => 'Other',
+        ],
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'options_select',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'inline',
+        'type' => 'list_default',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['group_id'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Group ID'))
+      ->setRequired(TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'numeric',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'inline',
+        'type' => 'numeric',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    return $fields;
+  }
+
+}
